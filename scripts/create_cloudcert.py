8b6192c8fd87c76e40ac35f0bc89e48dc8dfd5e6
kmwebnet <46954791+kmwebnet@users.noreply.github.com>
2020-11-25 18:19:53 +0900

    clarify license

--- scripts/create_cloudcert.py
+++ scripts/create_cloudcert.py
@@ -1,3 +1,25 @@
+# MIT License
+
+# Copyright (c) 2020 kmwebnet
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import os
 import base64
 import argparse
@@ -54,8 +76,8 @@ def create_server(cn, server_file, server_key_file, signer_file, signer_key_file
     # Create Server cert key pair
     print('\nServer Cert key')
     server_priv_key = load_or_create_key(server_key_file, backend=be)
-        
-    
+
+
     print('\nLoad Signer')
     # Load the Signing key from the file
     print('    Loading key from %s' % signer_key_file)
@@ -89,7 +111,7 @@ def create_server(cn, server_file, server_key_file, signer_file, signer_key_file
     builder = builder.serial_number(cert_sn(16, builder))
 
 
-    # Sign certificate 
+    # Sign certificate
     server_cert = builder.sign(private_key=signer_ca_priv_key, algorithm=hashes.SHA256(), backend=be)
 
     # Save certificate for reference
@@ -110,5 +132,5 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     create_server(args.cn, args.server, args.serverkey, args.signer, args.signerkey)
-    
+
     print('\nDone')
